56ca39d8e016823e47b4bed4998ca88a55406cf6
george827 <kinyanjuigeorge827@gmail.com>
2023-03-07 22:41:41 +0300

    fix test error

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require './person'
 
-class student < Person
-    def initialize(classroom)
-        super(age, name ='Unknown', parent_permission: true)
-        @classroom = classroom
-    end
+class Student < Person
+  def initialize(classroom)
+    super(age, name, parent_permission: true)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end

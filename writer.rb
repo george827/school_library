08f9fe654692ba203b5e23f4fd90a3239abe9c19
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-14 19:28:10 +0200

    Reduce complexity of write method

--- writer.rb
+++ writer.rb
@@ -3,33 +3,35 @@ require_relative 'book'
 require_relative 'rental'
 
 class Writter
-    def self.write(people, books, rentals)
-      files = {
-        "people.json" => people,
-        "rentals.json" => rentals,
-        "books.json" => books
-      }
+  def self.write(people, books, rentals)
+    files = {
+      'people.json' => people,
+      'rentals.json' => rentals,
+      'books.json' => books
+    }
 
-      files.each do |file, objects|
-        if File.exist?(file) and !File.zero?(file)
-          data = JSON.parse(File.read(file))
+    files.each do |file, objects|
+      if File.exist?(file) and !File.zero?(file)
+        data = JSON.parse(File.read(file))
 
-          data = data.map do |d|
-            if d.class == Person
-              Person.new(d['name'], d['age'], parent_permission: d['parent_permission'])
-            elsif d.class == Book
-              Book.new(d['title'], d['author'])
-            elsif d.class == Rental
-              Rental.new(d['date'], d['book'], d['person'])
-            end
-          end
+        data = Writter.morph(data)
 
-          objects = data + objects
-        end
+        objects = data + objects
+      end
 
-        File.open(file, "w") do |f|
-          f.write(JSON.pretty_generate(objects.map(&:to_json)))
-        end
+      File.write(file, JSON.pretty_generate(objects.map(&:to_json)))
+    end
+  end
+
+  def self.morph(data)
+    data.map do |d|
+      if d.instance_of?(Person)
+        Person.new(d['name'], d['age'], parent_permission: d['parent_permission'])
+      elsif d.instance_of?(Book)
+        Book.new(d['title'], d['author'])
+      elsif d.instance_of?(Rental)
+        Rental.new(d['date'], d['book'], d['person'])
       end
     end
+  end
 end
